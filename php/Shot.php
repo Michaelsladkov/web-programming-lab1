5cf1007d0687e687dde1ad44585ba8932f629895
Michaelsladkov <msladkov2002@gmail.com>
2021-08-24 20:45:45 +0300

    shot logic and fixed image

--- php/Shot.php
+++ php/Shot.php
@@ -1,1 +1,24 @@
 <?php
+$x = intval($_POST['X']);
+$y = intval($_POST['Y']);
+$r = intval($_POST['R']);
+echo check_shot($x, $y, $r);
+
+function check_shot($x, $y, $r) {
+    if ($x >= 0) {
+        if ($y >= 0) {
+            return $x * $x + $y * $y <= $r * $r;
+        }
+        else {
+            return $r + $y <= $x;
+        }
+    }
+    else {
+        if ($y > 0) {
+            return false;
+        }
+        else {
+            return $x >= (-$r) && $y >= (-$r) / 2;
+        }
+    }
+}
