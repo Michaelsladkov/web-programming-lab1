384570036396504db41f2704e51328c3026e4fcd
Michaelsladkov <msladkov2002@gmail.com>
2021-09-01 12:44:14 +0300

    prototype

--- php/Shot.php
+++ php/Shot.php
@@ -2,12 +2,43 @@
 if (!isset($_POST['X']) || !isset($_POST['Y']) || !isset($_POST['R'])) {
     exit("At least one parameter is not assigned");
 }
-$x = floatval($_POST['X']);
-$y = intval($_POST['Y']);
-$r = intval($_POST['R']);
+$x = $_POST['X'];
+$y = $_POST['Y'];
+$r = $_POST['R'];
+validate($x, $y, $r);
+$out = fopen("../storage", "a");
+fwrite($out, process_shot($x, $y, $r));
+fclose($out);
+draw_output();
+
+function validate($x, $y, $r) {
+    if (is_nan($x)) {
+        exit("X не число");
+    }
+    $x_num = floatval($x);
+    if ($x_num > 3 || $x_num < (-5)) {
+        exit("Число Х должно принадлежать отрезку (-5 ... 3)");
+    }
+    if (is_nan($y)) {
+        exit("Y не число");
+    }
+    if (is_nan($r)) {
+        exit("R не число");
+    }
+}
 
 function process_shot($x, $y, $r) {
-
+    $x_num = floatval($x);
+    $y_num = intval($y);
+    $r_num = intval($r);
+    $success = check_shot($x_num, $y_num, $r_num);
+    define("b", "<td>");
+    define("e", "</td>");
+    define("s", "</td><td>");
+    return b.$x.s.$y.s.$r.s.
+        date("G:i:s d-m-Y").s.
+        (round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 6)).
+        s.($success ? "попал" : "не попал").e.PHP_EOL;
 }
 
 function check_shot($x, $y, $r) {
@@ -27,4 +58,22 @@ function check_shot($x, $y, $r) {
             return $x >= (-$r) && $y >= (-$r) / 2;
         }
     }
+}
+
+function draw_output() {
+    $begin = file_get_contents("../html/output_template_begin.html");
+    $end = file_get_contents("../html/output_template_end.html");
+    $storage = fopen("../storage", "r");
+    $records = [];
+    while (!feof($storage)) {
+        array_push($records, fgets($storage));
+    }
+    echo $begin;
+    $length = count($records);;
+    for ($i = 0; $i < $length; $i++) {
+        echo "<tr>";
+        echo $records[$length - $i - 1];
+        echo "</tr>";
+    }
+    echo $end;
 }
