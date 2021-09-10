26a05a94b50a863d27287171b4cc25835b0898c3
Michaelsladkov <msladkov2002@gmail.com>
2021-09-10 13:34:01 +0300

    many values for one parameter test

--- php/Shot.php
+++ php/Shot.php
@@ -1,1 +1,8 @@
 <?php
+if (!isset($_POST['X']) || !isset($_POST['Y']) || !isset($_POST['R'])) {
+    exit("At least one parameter is not assigned");
+}
+$x = $_POST['X'];
+$y = $_POST['Y'];
+$r = $_POST['R'];
+echo $x."   ".$y."   ".$r;
