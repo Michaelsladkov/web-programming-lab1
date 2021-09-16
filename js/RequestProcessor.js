36571e8ca08c0df98e5c8c27a575c5c7d1381973
Michaelsladkov <msladkov2002@gmail.com>
2021-09-16 17:56:01 +0300

    paddings fixed

--- js/RequestProcessor.js
+++ js/RequestProcessor.js
@@ -1,12 +1,10 @@
 function makeRequest() {
     if (!validation()) return false;
-    console.log(`X=${getXNumberValue()}&Y=${getYNumberValue()}&R=${getRNumberValue()}`);
     $.ajax({
         url: "../php/Shot.php",
         type: "POST",
         data: `X=${getXNumberValue()}&Y=${getYNumberValue()}&R=${getRNumberValue()}`,
         success: function (response) {
-            console.log(response);
             let data = JSON.parse(response);
             appendLine(data);
         }
